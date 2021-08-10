5aca70faa63ceccd20a055f23db37f50f1fc3ad4
M. DeLoia <matthew.deloia@ngc.com>
2021-08-10 11:43:23 -0500

    Add files via upload

--- SoccerDraft_2021.R
+++ SoccerDraft_2021.R
@@ -10,41 +10,57 @@ library(lubridate)
 library(DT)
 library(janitor)
 
+words_remove <- "N/A|none|N/a|Na|no conflict|No conflicts|No conflicts.|none as of now| none|None.|None|No conflict|No Conflicts|n/a|No known conflicts.|Not sure yet|No known conflict at this time"
 
-#Load registration roster, assign leagues, join coaches information
-df <- read_xlsx("Test_Registration.xlsx") %>%
+#Load registration roster| assign leagues, join coaches information
+df <- read.csv("Registration_Aug8.csv") %>%
     clean_names() %>% 
-    mutate_at(vars(last_name, first_name), tolower) %>%
-    left_join(read_xlsx("Recreational Player Evaluations.xlsx", sheet = "Master") %>%
+    drop_na(player_id) %>% 
+    rename(last_name=player_last_name, 
+           first_name=player_first_name,
+           parent_email=account_email,
+           parent_phone=account_phone) %>%
+    mutate(player_id = tolower(paste(first_name, last_name, sep = "" ))) %>%
+    mutate(player_id = gsub(" |[.]|[-]|[']", "", player_id)) %>% 
+    mutate(parent_name = paste(account_first_name, account_last_name)) %>%
+    mutate(parent_phone = as.character(parent_phone)) %>% 
+    select(-c(address:paren_ta_s_approval_and_medical_release)) %>% 
+    left_join(read_xlsx("Recreational Player Evaluations 2021.xlsx", sheet = "Master") %>%
                   clean_names() %>%
-                  select(last_name, first_name, rating) %>% 
-                  mutate_at(vars(last_name, first_name), tolower) %>%
-                  drop_na(rating)) %>% 
-    mutate(birth_year= as.integer(format(birthday, format="%Y"))) %>% 
-    mutate(birthday = as.Date(birthday)) %>%
-    mutate(age= round(age_calc(birthday, enddate = Sys.Date(), units = "months")/12,1)) %>%
+                  mutate(player_id = tolower(paste(player_first_name, player_last_name, sep = "" ))) %>%
+                  mutate(player_id = gsub(" |[.]|[-]|[']", "", player_id)) %>% 
+                  select(player_id, rating)) %>% 
+    mutate(birth_date = as.Date(birth_date)) %>% 
+    mutate(birth_year= as.integer(format(birth_date, format="%Y"))) %>%
+    mutate(birth_year = if_else(player_id=="michellecorso", as.integer(2009), birth_year)) %>% 
+    mutate(age= round(age_calc(birth_date, enddate = Sys.Date(), units = "months")/12,1)) %>%
     mutate(league = if_else(birth_year >= 2017, "5U",
-    if_else(birth_year >=2016, "6U",
-    if_else(birth_year >= 2015, "7U",
-    if_else(birth_year >= 2014, "8U",
-    if_else(birth_year >= 2012, "10U",
-    if_else(birth_year >= 2010, "12U",
-    if_else(birth_year >= 2008, "14U",
-    if_else(birth_year >=2003, "19U", "Adult"))))))))) %>%
-    mutate(league = if_else(gender=="Male", paste("Boys",league, sep="_"), paste("Girls", league, sep = "_"))) %>% 
+                            if_else(birth_year >=2016, "6U",
+                                    if_else(birth_year >= 2015, "7U",
+                                            if_else(birth_year >= 2014, "8U",
+                                                    if_else(birth_year >= 2012, "10U",
+                                                            if_else(birth_year >= 2010, "12U",
+                                                                    if_else(birth_year >= 2008, "14U",
+                                                                            if_else(birth_year >=2003, "19U", "Adult"))))))))) %>%
+    mutate(league = if_else(gender=="M", paste("Boys",league, sep="_"), paste("Girls", league, sep = "_"))) %>% 
     mutate(league = gsub("Boys_19U", "Coed_19U", league)) %>% 
     mutate(league = gsub("Girls_19U", "Coed_19U", league)) %>% 
     mutate(league = gsub("Boys_Adult", "Coed_Adult", league)) %>% 
     mutate(league = gsub("Girls_Adult", "Coed_Adult", league)) %>%     
-    # left_join(read_xlsx("Test_Registration.xlsx", sheet = "Coaches") %>%
-    #               clean_names() %>% 
-    #               mutate_at(vars(coach_last, coach_first, last_name, first_name), tolower)) %>%
-    mutate(parent_coach = if_else(parent_coach =="", 0, 1)) %>% 
-    arrange(league, parent_coach,  rating, age) %>% 
+    mutate(head_coach = if_else(volunteer_head_coach =="", 0, 1)) %>% 
+    mutate(head_coach2 = if_else(head_coach==1, 6-rating, 0)) %>% 
+    rename(coach_email=volunteer_head_coach) %>%
+    rename(notes = practice_conflict_day_please_explain_conflict) %>% 
+    mutate(notes = gsub(words_remove, "", notes)) %>% 
+    arrange(league, -head_coach, -head_coach2,  -rating, -age) %>% 
     rownames_to_column("id") %>% 
     mutate(id = as.numeric(id)) %>% 
     group_by(league) %>% 
     mutate(Rank = rank(id))
+
+leagues <- c("5U", "6U", "7U", "8U", "10U", "12U", "14U", "19U", "Adult")
+gender <- c("Boys", "Girls", "Coed")
+league <- c("Boys_5U", "Girls_5U", "Boys_6U", "Girls_6U", "Boys_7U", "Girls_7U", "Boys_8U", "Girls_8U", "Boys_10U", "Girls_10U", "Boys_12U", "Girls_12U", "Boys_14U", "Girls_14U", "Coed_19U", "Coed_Adult")
 
 # Desired number of players per team
 target_5U <- 8
